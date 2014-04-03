07c7f63a772aa10e3dc5fc8ad4c696dbbdd2d775
Arni <arnidat@gmail.com>
2014-04-03 11:10:06 +0200

    >> q4 done << question4.py: Finished the code for q4. StatML_exam_lwf986.lyx: Finished the report for q4 (I think). Figures/*: Combined the two eigen plots into one plot. config.py: Combined the two eigen plots into one plot.

--- src/modules/config.py
+++ src/modules/config.py
@@ -14,8 +14,7 @@ savefigs=True
 
 # The following values define the paths where the different plots should be
 # saved to.
-fig2_4_1_esPath='../Report/Figures/2_4_1-eigenspectrum.png'
-fig2_4_2_eiPath='../Report/Figures/2_4_2-eigenImportance.png'
-fig2_4_3_scPath='../Report/Figures/2_4_3-scatter.png'
+fig2_4_1_esPath='../Report/Figures/2_4_1-combined_eigen.png'
+fig2_4_2_scPath='../Report/Figures/2_4_2-scatter.png'
 
 fig2_5_1_scPath='../Report/Figures/2_5_1-scatterandcenter.png'
